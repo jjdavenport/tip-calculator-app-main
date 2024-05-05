f57b9fc98f8ca04a71f1717d09a128efaa0bd77f
jjdavenport <jordanjamesdavenport@gmail.com>
2024-05-05 14:55:09 +0100

    custom function resets buttons if clicked

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 const reset = document.getElementById("reset");
 const tipA = document.getElementById("tip-amount");
-const tipP = document.getElementById("tip-per-person");
+const totalP = document.getElementById("total-per-person");
 const btn = document.querySelectorAll(".btn");
 const bill = document.getElementById("bill");
 const custom = document.getElementById("custom");
 const people = document.getElementById("people");
-
+const billValue = document.getElementById("bill").value;
+const customValue = document.getElementById("custom").value;
+const peopleValue = document.getElementById("people").value;
 let otherActive = null;
+const btn5 = 5;
 
 btn.forEach((btn) => {
   btn.addEventListener("click", (e) => {
@@ -14,8 +17,15 @@ btn.forEach((btn) => {
 
     if (otherActive && otherActive !== e.target) {
       otherActive.classList.remove("btn-active");
+      custom.value = "";
     }
     otherActive = e.target;
+  });
+});
+
+custom.addEventListener("input", () => {
+  btn.forEach((btn) => {
+    btn.classList.remove("btn-active");
   });
 });
 
@@ -26,4 +36,28 @@ reset.addEventListener("click", () => {
   bill.value = "";
   custom.value = "";
   people.value = "";
+  tipA.innerText = "$0.00";
+  totalP.innerText = "$0.00";
+});
+
+function tip() {}
+
+function total() {
+  let totalBill = bill / people;
+  totalP.innerText = `$${totalBill}`;
+}
+
+function tipAmount() {
+  let tipValue = bill / people;
+  tipA.innerText = `$${tipValue}`;
+}
+
+bill.addEventListener("input", () => {
+  total();
+  tipAmount();
+});
+
+people.addEventListener("input", () => {
+  total();
+  tipAmount();
 });
