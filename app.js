665d436e03f669bec05ba89b3276723b6459549f
jjdavenport <jordanjamesdavenport@gmail.com>
2024-05-11 18:27:55 +0100

    working functions yet to add custom/btn if/else error states and button styling

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const bill = document.getElementById("bill");
 const btn = document.querySelectorAll(".btn");
-let btnOutput = null;
-let billOutput = null;
-let customOutput = null;
-let peopleOutput = null;
-let totalBillPer = null;
+let btnOutput = 0;
+let billOutput = 0;
+let customOutput = 0;
+let peopleOutput = 0;
+let totalBillPer = 0;
 const custom = document.getElementById("custom");
 const people = document.getElementById("people");
+const tipAmount = document.getElementById("tip-amount");
+const totalPer = document.getElementById("total-per-person");
+const resetBtn = document.getElementById("reset");
 
 function btnValue() {
   btn.forEach((btn) => {
@@ -22,39 +25,50 @@ btnValue();
 
 bill.addEventListener("input", () => {
   billOutput = parseFloat(bill.value);
-  totalCustom();
-  tipCustom();
 });
 
 custom.addEventListener("input", () => {
   customOutput = parseFloat(custom.value) / 100;
+  totalCustom();
   tipCustom();
 });
 
 people.addEventListener("input", () => {
   peopleOutput = parseFloat(people.value);
   totalCustom();
+  tipCustom();
 });
 
 function totalBtn() {
   let tip = billOutput * btnOutput;
   let totalBill = billOutput + tip;
   totalBillPer = totalBill / peopleOutput;
+  totalPer.innerText = `$${totalBillPer}`;
 }
 
 function totalCustom() {
   let tip = billOutput * customOutput;
   totalBill = billOutput + tip;
   totalBillPer = totalBill / peopleOutput;
+  totalPer.innerText = `$${totalBillPer}`;
 }
 
 function tipBtn() {
-  let tip = billOutput * btnOutput;
-  totalTip = tip;
+  let totalTip = billOutput * btnOutput;
+  let tipCalc = totalTip / peopleOutput;
+  tipAmount.innerText = `$${tipCalc}`;
 }
 
 function tipCustom() {
-  let tip = billOutput * customOutput;
-  totalTip = tip;
-  console.log(totalTip);
+  let totalTip = billOutput * customOutput;
+  let tipCalc = totalTip / peopleOutput;
+  tipAmount.innerText = `$${tipCalc}`;
 }
+
+resetBtn.addEventListener("click", () => {
+  bill.value = "";
+  custom.value = "";
+  people.value = "";
+  totalPer.innerText = "$0.00";
+  tipAmount.innerText = "$0.00";
+});
